578878a0d3e216a3db6fe115b1f90221baadfeae
Loic Jouhannet <loic.jouhannet@gmail.com>
2016-02-17 08:36:09 +0100

    Exercises on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   # Save user email in lower case
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   # email regex pattern
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   # Set for an encrypted password
   has_secure_password
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -45,7 +45,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com form foo@bar..com]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -57,6 +57,12 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = duplicate_user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email should be saved in lower case" do
+    @user.email = mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
   # Tests for password validity on user
