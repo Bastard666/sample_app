41819f6b6e71612b12fc1b49ca4e621ac1f5a198
Loic Jouhannet <loic.jouhannet@gmail.com>
2016-02-19 23:16:15 +0100

    Log in/log out exercises

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,13 @@ class SessionsController < ApplicationController
 
   # Action for authentication and creation of a session
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = user ? 'Invalid email/password combination' : 'Unknown user'
+      flash.now[:danger] = @user ? 'Invalid email/password combination' : 'Unknown user'
       render 'new'
     end
   end
